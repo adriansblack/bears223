7e6897dbe21e2aed11f1292d666aef74e42b33d1
adrian <adriansblack@gmail.com>
2014-07-16 17:45:15 -0400

    login/registarion
    sylins

--- common.php
+++ common.php
@@ -1,10 +1,10 @@
 <?php 
 
     // These variables define the connection information for your MySQL database 
-    $username = "mentorbe"; 
-    $password = "0THoz03p9a"; 
+    $username = "root"; 
+    $password = "root"; 
     $host = "localhost"; 
-    $dbname = "mentorbe_123"; 
+    $dbname = "MentorTest"; 
 
     // UTF-8 is a character encoding scheme that allows you to conveniently store 
     // a wide varienty of special characters, like ¢ or €, in your database. 
