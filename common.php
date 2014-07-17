de2421402f2783e0ba722962572dbd5fd61bb265
Amanda Chen <amandac188@gmail.com>
2014-07-16 23:48:36 -0400

    working registration 2
    yay

--- common.php
+++ common.php
@@ -1,10 +1,10 @@
 <?php 
 
     // These variables define the connection information for your MySQL database 
-    $username = "root"; 
-    $password = "root"; 
+    $username = "mentorbe_ar"; 
+    $password = "startup123"; 
     $host = "localhost"; 
-    $dbname = "MentorTest"; 
+    $dbname = "mentorbe_mentorbear"; 
 
     // UTF-8 is a character encoding scheme that allows you to conveniently store 
     // a wide varienty of special characters, like ¢ or €, in your database. 
